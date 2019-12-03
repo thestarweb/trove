1f03e5645cc40ff3e93cd74a4efbb0dd98afc31b
star-work <57977810+star-work@users.noreply.github.com>
2019-12-03 17:10:34 +0800

    完善专精模块

--- script/mastery.js
+++ script/mastery.js
@@ -3,8 +3,9 @@ $("#mastery").get_attribute=function(){
 	var lv=$("#mastery_lv").value;
 	return {damage_:lv*0.15,max_health_:lv*0.5};
 }
-$("#mastery").onchange=function(){
+$("#mastery").oninput=function(){
 	if(this.update) this.update();
+	$("#mastery_bar div")[0].style.width=($("#mastery_lv_point").value/$("#mastery_need").innerHTML*100)+"%";
 }
 var get_mastery_lv_point=function(lv){//升级所需专精
 	if(lv<1) return 0;
@@ -12,9 +13,10 @@ var get_mastery_lv_point=function(lv){//升级所需专精
 	if(lv<10) return 50;
 	if(lv<20) return 75;
 	if(lv<300) return 100;
-	return Math.ceil((lv+1)/2);
+	return Math.ceil((parseInt(lv)+1)/2);
 }
-$("#mastery_lv").onchange=function(){
+$("#mastery_lv").oninput=function(){
+	if(this.value=="") return;
 	this.value=parseInt(this.value);
 	if(isNaN(this.value)) this.value=1;
 	else if(this.value<1)this.value=1;
@@ -22,9 +24,32 @@ $("#mastery_lv").onchange=function(){
 	for(var i=1;i<this.value;i++){
 		point+=get_mastery_lv_point(i);
 	}
-	$("#mastery_point").value=point;
+	var lv_point=get_mastery_lv_point(this.value)
+	$("#mastery_need").innerHTML=lv_point;
+	$("#mastery_point").value=point+parseInt($("#mastery_lv_point").value||0);
 }
-$("#mastery_point").onchange=function(){
+$("#mastery_lv").onchange=function(){
+	if($("#mastery_lv_point").value>=$("#mastery_need").innerHTML){
+		$("#mastery_lv_point").value=parseInt($("#mastery_need").innerHTML)-1;
+	}
+}
+$("#mastery_lv_point").oninput=function(){
+	$("#mastery_lv").oninput();
+}
+$("#mastery_lv_point").onchange=function(){
+		this.value=parseInt(this.value);
+		if(isNaN(this.value)) this.value=0;
+		if(this.value<0||this.value>get_mastery_lv_point($("#mastery_lv").value)){
+			var temp=parseInt(this.value);
+			this.value=0;
+			$("#mastery_lv").oninput();//算出当前专精总共需要多少
+			$("#mastery_point").value=parseInt($("#mastery_point").value)+temp;//然后和现在的值相加
+			$("#mastery_point").oninput();//再扔回去计算
+			$("#mastery").oninput();//通知更新
+		}
+}
+$("#mastery_point").oninput=function(){
+	if(this.value=="") return;
 	this.value=parseInt(this.value);
 	if(isNaN(this.value)) this.value=1;
 	else if(this.value<1)this.value=1;
@@ -35,7 +60,13 @@ $("#mastery_point").onchange=function(){
 		temp-=point;
 		lv++;
 	}
-	console.log(lv,temp);
 	$("#mastery_lv").value=lv;
+	$("#mastery_need").innerHTML=point;
+	$("#mastery_lv_point").value=temp;
 }
-$("#mastery").onchange();
+$("#mastery_lv").onkeydown=$("#mastery_lv_point").onkeydown=$("#mastery_point").onkeydown=function(ev){
+	if(ev.key&&ev.key.length==1&&!(ev.key>=0&&ev.key<=9)) return false;
+	//return true;
+}
+$("#mastery_lv").oninput();
+$("#mastery").oninput();
