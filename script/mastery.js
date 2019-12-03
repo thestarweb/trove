b74203ce54de1a95738dc15eaff862cf31683efe
star-work <57977810+star-work@users.noreply.github.com>
2019-12-03 13:50:08 +0800

    修正专精计算

--- script/mastery.js
+++ script/mastery.js
@@ -6,18 +6,36 @@ $("#mastery").get_attribute=function(){
 $("#mastery").onchange=function(){
 	if(this.update) this.update();
 }
-var get_mastery_lv_point=function(lv){
+var get_mastery_lv_point=function(lv){//升级所需专精
+	if(lv<1) return 0;
 	if(lv<5) return 25;
 	if(lv<10) return 50;
 	if(lv<20) return 75;
 	if(lv<300) return 100;
-	return lv/2;
+	return Math.ceil((lv+1)/2);
 }
 $("#mastery_lv").onchange=function(){
+	this.value=parseInt(this.value);
+	if(isNaN(this.value)) this.value=1;
+	else if(this.value<1)this.value=1;
 	var  point=0;
-	for(var i=1;i<=this.value;i++){
+	for(var i=1;i<this.value;i++){
 		point+=get_mastery_lv_point(i);
 	}
 	$("#mastery_point").value=point;
+}
+$("#mastery_point").onchange=function(){
+	this.value=parseInt(this.value);
+	if(isNaN(this.value)) this.value=1;
+	else if(this.value<1)this.value=1;
+	var lv=1;
+	var point;
+	var temp=this.value;
+	while(temp>=(point=get_mastery_lv_point(lv))){
+		temp-=point;
+		lv++;
+	}
+	console.log(lv,temp);
+	$("#mastery_lv").value=lv;
 }
 $("#mastery").onchange();
