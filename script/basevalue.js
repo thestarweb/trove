3fbdc38d6a0076e858f0ec6919b98db61557f54c
star-work <57977810+star-work@users.noreply.github.com>
2019-12-05 16:07:09 +0800

    修正几个数据

--- script/basevalue.js
+++ script/basevalue.js
@@ -3,7 +3,12 @@
 	dom.get_attribute=function(){
 		return{
 			"critical_damage":50,
-			"damage":350
+			"damage":350,
+			"jump":2,
+			"magic_find":50,
+			"attack_speed":100,
+			"move_speed":40,
+			"max_energy":100
 		};
 	}
 	if(dom.update) dom.update();
