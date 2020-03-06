9b6e23fbbeb78d5113cada1a21ffe3b3a77acc24
star-work <57977810+star-work@users.noreply.github.com>
2020-03-06 11:13:51 +0800

    增加宝石检测页面

---
+++ control/gem_control.php
@@ -0,0 +1,9 @@
+<?php
+namespace strove; 
+class gem_control{
+	public function check_page($system){
+		$system->show_head($system->lang('gem','check.title').' by star_ss');
+		include $system->get_view('gem/check');
+		$system->show_foot();
+	}
+}
